77e18e25d8b3ac011064cfbeda765beeef90942d
konstantinBalakin <upgrade81@mail.ru>
2023-07-04 23:50:42 +0300

    задача 2

--- recepts.py
+++ recepts.py
@@ -15,7 +15,7 @@ with open('recept.txt', encoding='utf-8') as f:
             ingredient_info = f.readline().strip().split(" | ")
             ingredient_name, quantity, measure = ingredient_info
             ingredient = {
-                'ingridient_name': ingredient_name,
+                'ingredient_name': ingredient_name,
                 'quantity': int(quantity),
                 'measure': measure
             }
@@ -28,21 +28,37 @@ with open('recept.txt', encoding='utf-8') as f:
         # Пропускаем пустую строку между блюдами
         f.readline()
 
-# print (cook_book)
+    print (cook_book)
+    print()
+    print()
     
-person_count = 4
 
-dishes = list(cook_book.values())
 
-def get_shop_list_by_dishes(dishes_for, person_count):
-    dict1 = {}
-    for dish in dishes_for:
-        if dish in dishes:
-        
-            dict1 += {'ingredient_name': {'measure': measure, quantity: quantity}
-                       }
-    print (dict1)
-    return dict1
 
-        
-get_shop_list_by_dishes(['Омлет'], 3)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            ingredients = cook_book[dish]
+            for ingredient in ingredients:
+                ingredient_name = ingredient['ingredient_name']
+                measure = ingredient['measure']
+                quantity = ingredient['quantity']*person_count
+                if ingredient_name not in shop_list:
+                    shop_list[ingredient_name] = {'quantity': quantity, 'measure': measure}
+                else:
+                    shop_list[ingredient_name][quantity]+=quantity
+
+        else:
+            print(f' блюдо {dish} отсутсвует в cook_book')
+    
+    return shop_list
+
+dishes = ['Омлет', 'Запеченный картофель', 'пирог']        
+result = get_shop_list_by_dishes(dishes, 5)
+
+for ingredient_name, ingredient_info in result.items():
+    quantity = ingredient_info['quantity']
+    measure = ingredient_info['measure']
+    print(f"{ingredient_name}: {quantity} {measure}")
