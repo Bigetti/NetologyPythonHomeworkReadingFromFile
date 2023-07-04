1e883a69f317b5b53f8db0da51355f35031a22e1
bigetti <upgrade81@mail.ru>
2023-07-04 21:53:55 +0300

    started second task

--- recepts.py
+++ recepts.py
@@ -28,5 +28,21 @@ with open('recept.txt', encoding='utf-8') as f:
         # Пропускаем пустую строку между блюдами
         f.readline()
 
-print (cook_book)
+# print (cook_book)
     
+person_count = 4
+
+dishes = list(cook_book.values())
+
+def get_shop_list_by_dishes(dishes_for, person_count):
+    dict1 = {}
+    for dish in dishes_for:
+        if dish in dishes:
+        
+            dict1 += {'ingredient_name': {'measure': measure, quantity: quantity}
+                       }
+    print (dict1)
+    return dict1
+
+        
+get_shop_list_by_dishes(['Омлет'], 3)
